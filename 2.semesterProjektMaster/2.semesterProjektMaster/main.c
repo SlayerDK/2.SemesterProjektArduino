26bae601137f71dd3d39d971c5ae2bed0b66acb9
SkayerDK <49123298+SkayerDK@users.noreply.github.com>
2024-11-20 17:05:22 +0100

    Micro chip studio master projekt med stepper motor og lys kode

---
+++ 2.semesterProjektMaster/2.semesterProjektMaster/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ * Created: 11/20/2024 12:18:36 PM
+ *  Author: christianwinther
+ */ 
+
+#include <xc.h>
+#include "motor.h"
+
+int main(void)
+{
+    motor_setup();
+	step(5000, 1);
+	step(-5000, 1);
+	_delay_ms(5000);
+	step(5000, 0);
+	step(-5000, 0);
+}
