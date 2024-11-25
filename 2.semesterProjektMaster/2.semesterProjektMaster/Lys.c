d985df25b84aabac2bcc08d72840de94fa7bfab8
pilotmul <jesper19072000@gmail.com>
2024-11-25 10:17:00 +0100

    Kommunikations kode

--- 2.semesterProjektMaster/2.semesterProjektMaster/Lys.c
+++ 2.semesterProjektMaster/2.semesterProjektMaster/Lys.c
@@ -1,16 +1,16 @@
 #include "Lys.h"
 
-// Initialiser UART med den ønskede baud rate
+// Initialiser UART med den ï¿½nskede baud rate
 void uart_init(unsigned int ubrr) {
-	UBRR0H = (unsigned char)(ubrr >> 8);  // Sæt øvre byte af baud rate
-	UBRR0L = (unsigned char)ubrr;        // Sæt nedre byte af baud rate
-	UCSR0B = (1 << RXEN0) | (1 << TXEN0); // Aktivér transmitter og receiver
+	UBRR0H = (unsigned char)(ubrr >> 8);  // Sï¿½t ï¿½vre byte af baud rate
+	UBRR0L = (unsigned char)ubrr;        // Sï¿½t nedre byte af baud rate
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0); // Aktivï¿½r transmitter og receiver
 	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // 8 data bits, 1 stop bit
 }
 
 // Send en karakter via UART
 void uart_transmit(unsigned char data) {
-	while (!(UCSR0A & (1 << UDRE0)));  // Vent på, at bufferen er klar
+	while (!(UCSR0A & (1 << UDRE0)));  // Vent pï¿½, at bufferen er klar
 	UDR0 = data;  // Send data
 }
 
@@ -24,28 +24,29 @@ void uart_send_string(const char* str) {
 // Initialiser ADC med AVcc som reference og prescaler til 128
 void adc_init(void) {
 	ADMUX = (1 << REFS0);  // Brug AVcc som reference
-	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);  // Aktivér ADC og prescaler 128
+	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);  // Aktivï¿½r ADC og prescaler 128
 }
 
-// Generel funktion til at læse analog værdi fra en specificeret kanal (0-7)
+// Generel funktion til at lï¿½se analog vï¿½rdi fra en specificeret kanal (0-7)
 uint16_t read_analog(uint8_t channel) {
 	//channel 0 = top, 1 = bottom, 2 = left, 3 = right
-	channel &= 0x07;  // Begræns kanalen til 0-7
-	ADMUX = (ADMUX & 0xF0) | channel;  // Vælg den ønskede kanal
+	channel &= 0x07;  // Begrï¿½ns kanalen til 0-7
+	ADMUX = (ADMUX & 0xF0) | channel;  // Vï¿½lg den ï¿½nskede kanal
 	ADCSRA |= (1 << ADSC);  // Start konvertering
-	while (ADCSRA & (1 << ADSC));  // Vent på afslutning
-	return ADC;  // Returnér ADC værdi
+	while (ADCSRA & (1 << ADSC));  // Vent pï¿½ afslutning
+	return ADC;  // Returnï¿½r ADC vï¿½rdi
 }
 
 void getSteps(int16_t *moveVertical, int16_t *moveHorizontal) {
 	//vertical negative = move down, vertical positive = move up
 	//horizontal negative = move left, horizontal positive = move right
+	//Jo mere lys jo lavere vÃ¦rdi
 	uart_init(MYUBRR);  // Initialiser UART
 	adc_init();  // Initialiser ADC
 
 	uint16_t top, bottom, left, right;
 	uint16_t const correctionSize = 10;
-	//Læser alle sensors
+	//Lï¿½ser alle sensors
 	top = read_analog(0);
 	bottom = read_analog(1);
 	left = read_analog(2);
---
+++ 2.semesterProjektMaster/2.semesterProjektMaster/kommunikation.h
@@ -0,0 +1,9 @@
+#ifndef UART_UTILS_H
+#define UART_UTILS_H
+
+#include <stdint.h>
+
+// Funktionsprototyper
+void ConvertAndSendValues(int watt, int sol);
+
+#endif // UART_UTILS_H
